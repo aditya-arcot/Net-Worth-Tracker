2892f5e078bd37fc5f6b42096cca105ecf212175
aditya-arcot <arcot.aditya@utexas.edu>
2024-04-06 11:58:18 -0500

    add graphs

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from datetime import datetime
 import json
+import matplotlib.pyplot as plt
 
 data_filename = "data.json"
-main_options = ["Add data point", "Create graph", "Exit"]
+main_options = ["Add data point", "Create graphs", "Exit"]
 account_selection_options = ["Select a tracked account", "Add a new account"]
 
 
@@ -84,15 +85,65 @@ def addDataPoint():
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-def createGraph():
-    pass
+def createGraphs():
+    if not os.path.exists(data_filename):
+        print("Add some data and try again\n")
+        return
+    
+    with open(data_filename) as f:
+        data = json.load(f)
+    
+    dates = set()
+    accounts_data = {}
 
+    for account, balances in data.items():
+        for date in balances.keys():
+            dates.add(datetime.strptime(date, "%Y-%m-%d"))
+    dates = sorted(dates)
+
+    for account, balances in data.items():
+        accounts_data[account] = []
+        prev_balance = 0
+        for date in dates:
+            if date.strftime("%Y-%m-%d") in balances:
+                prev_balance = balances[date.strftime("%Y-%m-%d")]
+            accounts_data[account].append((date, prev_balance))
+
+    plt.figure(dpi=300, figsize=(15, 7.5))
+    for account, balances in accounts_data.items():
+        dates, values = zip(*sorted(balances))
+        plt.plot(dates, values, label=account)
+    plt.xlabel('Date')
+    plt.ylabel('Balance')
+    plt.title('Account Balances')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('account_balances')
+    plt.close()
+
+    running_totals = {}
+    for date in dates:
+        running_totals[date] = 0
+        for account, balances in accounts_data.items():
+            for _date, balance in balances:
+                if _date == date:
+                    running_totals[date] += balance
+                    break
+    dates, values = zip(*sorted(running_totals.items()))
+    plt.figure(dpi=300, figsize=(15, 7.5))
+    plt.plot(dates, values)
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.title('Net Worth')
+    plt.tight_layout()
+    plt.savefig('net_worth')
+    plt.close()
 
 while True:
     match parseOption(main_options):
         case 0:
             addDataPoint()
         case 1:
-            createGraph()
+            createGraphs()
         case 2:
             exit("Bye")
